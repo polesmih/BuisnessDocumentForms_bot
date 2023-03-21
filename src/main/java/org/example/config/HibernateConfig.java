371ea1dde0e4507cc1ddcfb8e93ee32a713688c9
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-22 01:17:21 +0600

    add HibernateConfig class

---
+++ src/main/java/org/example/config/HibernateConfig.java
@@ -0,0 +1,32 @@
+package org.example.config;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateConfig {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
+            Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
+
+    public static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+}
